a66f48142329cce06e45d2fe9c4d934065ab42b3
sakshi-1 <sakshis.ug18.ec@nitp.ac.in>
2020-07-28 23:45:23 +0530

    Updates

---
+++ precache-manifest.0087b4fa91578ca208d5597904d11bde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03f145ccb914f0f7ba9606738c64073",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f1977e1b3e6db539652c",
+    "url": "/monsters-rolodex/static/css/main.8a7e29c3.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/monsters-rolodex/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1977e1b3e6db539652c",
+    "url": "/monsters-rolodex/static/js/main.f655b902.chunk.js"
+  },
+  {
+    "revision": "ac7692f361dea471e0d6",
+    "url": "/monsters-rolodex/static/js/runtime-main.ab61569e.js"
+  }
+]);
